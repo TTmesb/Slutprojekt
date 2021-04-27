f52b131f803f8a3e7aa2a10991bf054bc30f951d
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-04-27 11:10:52 +0200

    Skapade frame samt datum i konsol

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.time.LocalDate;
 /**
  * Created 2021-04-27
  *
@@ -6,5 +8,22 @@
 public class Main {
     public static void main(String[] args) {
         // Här startas ditt program
+        // Design av framen
+        JFrame frame = new JFrame("Vecka.nu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(350,350);
+        frame.setIconImage(new ImageIcon("src/a.png").getImage());
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Delar av framen
+
+        JLabel nummer = new JLabel("Hej");
+        LocalDate hej = LocalDate.now();
+        nummer.setText(hej.toString());
+        System.out.println(hej);
+
+        frame.setVisible(true);
+
     }
 }
