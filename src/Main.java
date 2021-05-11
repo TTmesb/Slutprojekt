59d2751db8c81da03d3a33a3341293677e3f0b12
TTmesb <TTmesb@users.noreply.github.com>
2021-05-11 11:25:29 +0200

    star metod

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
-import java.util.Calendar;
+import java.util.LinkedList;
 
 /**
  * Created 2021-04-27
@@ -13,36 +15,61 @@ public class Main {
     public static void main(String[] args) {
         // Här startas ditt program
         // Design av framen
-        JFrame frame = new JFrame("Vecka.nu");
+        final MyPanel panel = new MyPanel();
+        final JFrame frame = new JFrame("Vecka.nu");
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(350,350);
         frame.setIconImage(new ImageIcon("src/a.png").getImage());
         JFrame.setDefaultLookAndFeelDecorated(true);
 
         //Delar av framen
 
-        JLabel vecka = new JLabel("",SwingConstants.CENTER);
-        JLabel manad = new JLabel("",SwingConstants.RIGHT);
-        JLabel år = new JLabel("",SwingConstants.LEFT);
+        JLabel vecka = new JLabel();
 
         LocalDate datum = LocalDate.now();
         vecka.setPreferredSize(new Dimension(350,350));
         vecka.setFont(new Font("Arial, serif", Font.BOLD,260));
         vecka.setForeground(new Color(28, 123, 183));
 
-        manad.setForeground(new Color(28, 123, 183));
-        manad.setFont(new Font("Arial, serif", Font.BOLD,260));
-        manad.setPreferredSize(new Dimension(100,100));
         int veckanu = datum.get(ChronoField.ALIGNED_WEEK_OF_YEAR)-1;
-        int manadnu = datum.get(ChronoField.DAY_OF_MONTH);
-        vecka.setText(String.valueOf(veckanu));
-        manad.setText(String.valueOf(manadnu));
-        frame.add(vecka);
-        frame.add(manad);
 
-        frame.getContentPane().setBackground(new Color(144, 192, 222));
+        vecka.setText(String.valueOf(veckanu));
+        panel.add(vecka);
+        frame.add(panel);
+        // frame.add(vecka);
+        panel.getContentPane();
         frame.pack();
         frame.setVisible(true);
-
     }
+        public static class MyPanel extends JPanel {
+        private LinkedList<star> stars;
+        public MyPanel(){
+            stars = new LinkedList<>();
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    stars.add(new star(e.getX(),e.getY()));
+                    MyPanel.this.repaint();
+                }
+            });
+
+        }
+
+            @Override
+            public void paintComponent(final Graphics g) {
+                super.paintComponent(g);
+                stars.stream().forEach((e)->e.render(g));
+                g.setColor(Color.black);
+                g.fillOval(0,0,40,40);
+
+            }
+
+            public Dimension getPreferredSize() {
+                return new Dimension(400, 400);
+            }
+
+            public void getContentPane() {
+            setBackground(new Color(144, 192, 222));
+            }
+        }
 }
---
+++ src/star.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class star {
+    private final int x;
+    private final int y;
+
+    public star(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
